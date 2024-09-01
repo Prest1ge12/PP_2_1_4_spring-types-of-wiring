89c43d5d28ad8ecd760932689428e317e2e8c9e3
Sergey Angelov <mrangel@yandex.ru>
2024-09-01 13:57:22 +0300

    Готовая задача!

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -6,10 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        KoscheiTheDeathless koscheiTheDeathless =
-                applicationContext.getBean(KoscheiTheDeathless.class);
+        // На свете есть океан , на океане остров , на острове дерево , на дереве заяц , в зайце утка , в утке яйцо , в яйце иголка , смерть Кощея на игле
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        KoscheiTheDeathless koscheiTheDeathless = applicationContext.getBean(KoscheiTheDeathless.class);
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
